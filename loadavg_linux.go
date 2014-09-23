4002b3588a8fede1279b2a218193e239cbaf68d9
Rafael Casado Sanchez <rafacas@gmail.com>
2014-09-23 16:35:40 +0200

    Changed LoadAvg type from map[string]float64 to struct

--- loadavg_linux.go
+++ loadavg_linux.go
@@ -9,37 +9,38 @@ import (
 )
 
 // LoadAvg represents the load average of the system
-// The following are the keys of the map:
-// Avg1  - The average processor workload of the last minute
-// Avg5  - The average processor workload of the last 5 minutes
-// Avg15 - The average processor workload of the last 15 minutes
-type LoadAvg map[string]float64
+type LoadAvg struct {
+	Avg1  float64 // The average processor workload of the last minute
+	Avg5  float64 // The average processor workload of the last 5 minutes
+	Avg15 float64 // The average processor workload of the last 15 minutes
+}
 
 // getLoadAvg gets the load average of a linux system from the
 // file /proc/loadavg.
 func getLoadAvg() (loadAvg LoadAvg, err error) {
 	file, err := ioutil.ReadFile("/proc/loadavg")
 	if err != nil {
-		return nil, err
+		return LoadAvg{}, err
 	}
 	content := string(file[:len(file)])
 
 	loadAvg = LoadAvg{}
 	fields := strings.Fields(content)
-	for i := 0; i < 3; i++ {
-		load, err := strconv.ParseFloat(fields[i], 64)
-		if err != nil {
-			return nil, err
-		}
-		switch i {
-		case 0:
-			loadAvg[`Avg1`] = load
-		case 1:
-			loadAvg[`Avg5`] = load
-		case 2:
-			loadAvg[`Avg15`] = load
-		}
+	loadAvg1, err := strconv.ParseFloat(fields[0], 64)
+	if err != nil {
+		return LoadAvg{}, err
 	}
+	loadAvg.Avg1 = loadAvg1
+	loadAvg5, err := strconv.ParseFloat(fields[1], 64)
+	if err != nil {
+		return LoadAvg{}, err
+	}
+	loadAvg.Avg5 = loadAvg5
+	loadAvg15, err := strconv.ParseFloat(fields[2], 64)
+	if err != nil {
+		return LoadAvg{}, err
+	}
+	loadAvg.Avg15 = loadAvg15
 
 	return loadAvg, nil
 }
